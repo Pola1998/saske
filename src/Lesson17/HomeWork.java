fab1458d2a7c0083764d30a78eaf158b0d562a17
John <serg.snow.muratov@mail.ru>
2023-06-12 22:22:00 +0300

    aaa

--- src/Lesson17/HomeWork.java
+++ src/Lesson17/HomeWork.java
@@ -1,7 +1,7 @@
 package Lesson17;
 
 public class HomeWork {
-    public static void main(String[] args) {
+    public static   void main(String[] args) {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
 
